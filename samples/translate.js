1ae176212549169db2e11e5a570a7b31c162e07f
Praveen Kumar Singh <v-praveen-singh@justenergy.com>
2018-10-23 20:08:49 +0530

    removed unnecessary extra white space

--- samples/translate.js
+++ samples/translate.js
@@ -150,64 +150,64 @@ function translateTextWithModel(text, target, model) {
 }
 
 require(`yargs`)
-    .demand(1)
-    .command(
-        `detect <text..>`,
-        `Detects the language of one or more strings.`,
-        {},
-        opts => detectLanguage(opts.text)
-    )
-    .command(
-        `list [target]`,
-        `Lists available translation languages. To language names in a language other than English, specify a target language.`,
-        {},
-        opts => {
-            if (opts.target) {
-                listLanguagesWithTarget(opts.target);
-            } else {
-                listLanguages();
-            }
-        }
-    )
-    .command(
-        `translate <toLang> <text..>`,
-        `Translates one or more strings into the target language.`,
-        {},
-        opts => translateText(opts.text, opts.toLang)
-    )
-    .command(
-        `translate-with-model <toLang> <model> <text..>`,
-        `Translates one or more strings into the target language using the specified model.`,
-        {},
-        opts => translateTextWithModel(opts.text, opts.toLang, opts.model)
-    )
-    .example(`node $0 detect "Hello world!"`, `Detects the language of a string.`)
-    .example(
-        `node $0 detect "Hello world!" "Goodbye"`,
-        `Detects the languages of multiple strings.`
-    )
-    .example(
-        `node $0 list`,
-        `Lists available translation languages with names in English.`
-    )
-    .example(
-        `node $0 list es`,
-        `Lists available translation languages with names in Spanish.`
-    )
-    .example(
-        `node $0 translate ru "Good morning!"`,
-        `Translates a string into Russian.`
-    )
-    .example(
-        `node $0 translate ru "Good morning!" "Good night!"`,
-        `Translates multiple strings into Russian.`
-    )
-    .example(
-        `node $0 translate-with-model ru nmt "Good morning!" "Good night!"`,
-        `Translates multiple strings into Russian using the Premium model.`
-    )
-    .wrap(120)
-    .recommendCommands()
-    .epilogue(`For more information, see https://cloud.google.com/translate/docs`)
-    .help()
-    .strict().argv;
+  .demand(1)
+  .command(
+    `detect <text..>`,
+    `Detects the language of one or more strings.`,
+    {},
+    opts => detectLanguage(opts.text)
+  )
+  .command(
+    `list [target]`,
+    `Lists available translation languages. To language names in a language other than English, specify a target language.`,
+    {},
+    opts => {
+      if (opts.target) {
+        listLanguagesWithTarget(opts.target);
+      } else {
+        listLanguages();
+      }
+    }
+  )
+  .command(
+    `translate <toLang> <text..>`,
+    `Translates one or more strings into the target language.`,
+    {},
+    opts => translateText(opts.text, opts.toLang)
+  )
+  .command(
+    `translate-with-model <toLang> <model> <text..>`,
+    `Translates one or more strings into the target language using the specified model.`,
+    {},
+    opts => translateTextWithModel(opts.text, opts.toLang, opts.model)
+  )
+  .example(`node $0 detect "Hello world!"`, `Detects the language of a string.`)
+  .example(
+    `node $0 detect "Hello world!" "Goodbye"`,
+    `Detects the languages of multiple strings.`
+  )
+  .example(
+    `node $0 list`,
+    `Lists available translation languages with names in English.`
+  )
+  .example(
+    `node $0 list es`,
+    `Lists available translation languages with names in Spanish.`
+  )
+  .example(
+    `node $0 translate ru "Good morning!"`,
+    `Translates a string into Russian.`
+  )
+  .example(
+    `node $0 translate ru "Good morning!" "Good night!"`,
+    `Translates multiple strings into Russian.`
+  )
+  .example(
+    `node $0 translate-with-model ru nmt "Good morning!" "Good night!"`,
+    `Translates multiple strings into Russian using the Premium model.`
+  )
+  .wrap(120)
+  .recommendCommands()
+  .epilogue(`For more information, see https://cloud.google.com/translate/docs`)
+  .help()
+  .strict().argv;
