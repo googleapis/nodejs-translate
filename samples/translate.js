25fe854fc90a6aa528233cb8b05764391537f01a
Praveen Kumar Singh <v-praveen-singh@justenergy.com>
2018-10-25 16:18:04 +0530

    replace promise callback with async await and place,error handling@yargs

--- samples/translate.js
+++ samples/translate.js
@@ -146,74 +146,69 @@ async function translateTextWithModel(text, target, model) {
   // [END translate_text_with_model]
 }
 
-async function main() {
-  require(`yargs`)
-    .demand(1)
-    .command(
-      `detect <text..>`,
-      `Detects the language of one or more strings.`,
-      {},
-      opts => detectLanguage(opts.text)
-    )
-    .command(
-      `list [target]`,
-      `Lists available translation languages. To language names in a language other than English, specify a target language.`,
-      {},
-      opts => {
-        if (opts.target) {
-          listLanguagesWithTarget('opts.target');
-        } else {
-          listLanguages();
-        }
+require(`yargs`)
+  .demand(1)
+  .command(
+    `detect <text..>`,
+    `Detects the language of one or more strings.`,
+    {},
+    async opts => await detectLanguage(opts.text).catch(console.error)
+  )
+  .command(
+    `list [target]`,
+    `Lists available translation languages. To language names in a language other than English, specify a target language.`,
+    {},
+    async opts => {
+      if (opts.target) {
+        await listLanguagesWithTarget('opts.target').catch(console.error);
+      } else {
+        await listLanguages().catch(console.error);
       }
-    )
-    .command(
-      `translate <toLang> <text..>`,
-      `Translates one or more strings into the target language.`,
-      {},
-      opts => translateText(opts.text, opts.toLang)
-    )
-    .command(
-      `translate-with-model <toLang> <model> <text..>`,
-      `Translates one or more strings into the target language using the specified model.`,
-      {},
-      opts => translateTextWithModel(opts.text, opts.toLang, opts.model)
-    )
-    .example(
-      `node $0 detect "Hello world!"`,
-      `Detects the language of a string.`
-    )
-    .example(
-      `node $0 detect "Hello world!" "Goodbye"`,
-      `Detects the languages of multiple strings.`
-    )
-    .example(
-      `node $0 list`,
-      `Lists available translation languages with names in English.`
-    )
-    .example(
-      `node $0 list es`,
-      `Lists available translation languages with names in Spanish.`
-    )
-    .example(
-      `node $0 translate ru "Good morning!"`,
-      `Translates a string into Russian.`
-    )
-    .example(
-      `node $0 translate ru "Good morning!" "Good night!"`,
-      `Translates multiple strings into Russian.`
-    )
-    .example(
-      `node $0 translate-with-model ru nmt "Good morning!" "Good night!"`,
-      `Translates multiple strings into Russian using the Premium model.`
-    )
-    .wrap(120)
-    .recommendCommands()
-    .epilogue(
-      `For more information, see https://cloud.google.com/translate/docs`
-    )
-    .help()
-    .strict().argv;
-}
-
-main().catch(console.error);
+    }
+  )
+  .command(
+    `translate <toLang> <text..>`,
+    `Translates one or more strings into the target language.`,
+    {},
+    async opts =>
+      await translateText(opts.text, opts.toLang).catch(console.error)
+  )
+  .command(
+    `translate-with-model <toLang> <model> <text..>`,
+    `Translates one or more strings into the target language using the specified model.`,
+    {},
+    async opts =>
+      await translateTextWithModel(opts.text, opts.toLang, opts.model).catch(
+        console.error
+      )
+  )
+  .example(`node $0 detect "Hello world!"`, `Detects the language of a string.`)
+  .example(
+    `node $0 detect "Hello world!" "Goodbye"`,
+    `Detects the languages of multiple strings.`
+  )
+  .example(
+    `node $0 list`,
+    `Lists available translation languages with names in English.`
+  )
+  .example(
+    `node $0 list es`,
+    `Lists available translation languages with names in Spanish.`
+  )
+  .example(
+    `node $0 translate ru "Good morning!"`,
+    `Translates a string into Russian.`
+  )
+  .example(
+    `node $0 translate ru "Good morning!" "Good night!"`,
+    `Translates multiple strings into Russian.`
+  )
+  .example(
+    `node $0 translate-with-model ru nmt "Good morning!" "Good night!"`,
+    `Translates multiple strings into Russian using the Premium model.`
+  )
+  .wrap(120)
+  .recommendCommands()
+  .epilogue(`For more information, see https://cloud.google.com/translate/docs`)
+  .help()
+  .strict().argv;
