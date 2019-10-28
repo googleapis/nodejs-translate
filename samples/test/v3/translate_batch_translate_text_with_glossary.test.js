2c9864e9fabfbbd7d1bb0da9190215f64fdf1428
Benjamin Coe <bencoe@google.com>
2019-10-28 16:19:09 -0700

    chore: address flaky test in flaky test PR

--- samples/test/v3/translate_batch_translate_text_with_glossary.test.js
+++ samples/test/v3/translate_batch_translate_text_with_glossary.test.js
@@ -71,6 +71,7 @@ describe(REGION_TAG, () => {
     await operation.promise();
   });
 
+  // see: https://github.com/googleapis/nodejs-translate/issues/364
   it.skip('should batch translate the input text with a glossary [FLAKY]', async () => {
     const projectId = await translationClient.getProjectId();
     const inputUri = `gs://cloud-samples-data/translation/text.txt`;
