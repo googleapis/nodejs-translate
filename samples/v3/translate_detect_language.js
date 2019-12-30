6d59e29ec8fa2cbc038884799c52acbd280bad25
bradmiro <bmiro@google.com>
2019-12-30 17:58:26 -0500

    Suppressed unhandled promise rejection errors

--- samples/v3/translate_detect_language.js
+++ samples/v3/translate_detect_language.js
@@ -39,14 +39,17 @@ function main(
       parent: `projects/${projectId}/locations/${location}`,
       content: text,
     };
+    try {
+      // Run request
+      const [response] = await translationClient.detectLanguage(request);
 
-    // Run request
-    const [response] = await translationClient.detectLanguage(request);
-
-    console.log(`Detected Languages:`);
-    for (const language of response.languages) {
-      console.log(`Language Code: ${language.languageCode}`);
-      console.log(`Confidence: ${language.confidence}`);
+      console.log(`Detected Languages:`);
+      for (const language of response.languages) {
+        console.log(`Language Code: ${language.languageCode}`);
+        console.log(`Confidence: ${language.confidence}`);
+      }
+    } catch (error) {
+      console.error(error.details);
     }
   }
 
